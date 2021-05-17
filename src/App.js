4c93f38b616aaf2f1af66e856a36ba7a3dc61d60
Travis <tleifj@gmail.com>
2021-05-16 21:39:40 -0700

    Adds a test component and adds Delete and Submit buttons with some basic events and state updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
-function App() {
+class App extends React.Component {
+  state = {
+    guess:  'guess'
+  };
+  checkGuess = () => {
+    this.setState({guess: 'new guess'});
+    console.log(this.state);
+  };
+  deleteFromGuess = () => {
+    let guess = this.state.guess;
+    let newGuess = guess.slice(0, -1);
+    this.setState({guess: newGuess});
+  }
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Happy Bee</h1>
-        
-      </header>
+      <Header title="Test Header"></Header>
+      <p>{this.state.guess}</p>
+      <button onClick={this.deleteFromGuess}>Delete</button>
+      <button onClick={this.checkGuess}>Submit</button>
     </div>
   );
+    }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,4 @@
+
+export default function Header({title}) {
+    return <header>{title}</header>
+}
